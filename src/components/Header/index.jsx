30dc2087722e5dc4187248295d50a8f4dc63b07e
mariam isa <maryaam1998@hotmail.com>
2026-04-08 23:42:51 -0400

    activate btn shop now

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../Button'
 import styles from './styles.module.css'
 
 export default function Header() {
+	const navigate = useNavigate();
+
 	return (
 		<div className={styles.headerContainer}>
 			<div className={styles.textContainer}>
 				<h1 className={styles.title}>Discover Your Style</h1>
 				<p className={styles.description}>Curated clothing & accessories for every mood, every moment.</p>
-				<Button text="Shop Now" />
+				<Button text="Shop Now" onClick={() => navigate('/shop')} />
 			</div>
 		</div>
 	)
